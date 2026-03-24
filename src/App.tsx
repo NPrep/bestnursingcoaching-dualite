fc0e81a49d4495c7a2c19fced9a46df252562346
NPrep <administration@nprep.in>
2026-03-24 15:17:43 +0530

    Revert "Codex-generated pull request"

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { RouterWrapper } from './next/RouterWrapper';
 
+// Pages
 import { Home } from './pages/Home';
+import { ExamDirectory } from './pages/ExamDirectory';
+import { ExamDetail } from './pages/ExamDetail';
+import { CoachingComparison } from './pages/CoachingComparison';
+import { Courses } from './pages/Courses';
+import { PYQHub } from './pages/PYQHub';
+import { TestSeries } from './pages/TestSeries';
 import { BlogList } from './pages/BlogList';
-import { StaticPage } from './pages/StaticPage';
+import { BlogPost } from './pages/BlogPost';
 
 type AppProps = {
   initialPath?: string;
@@ -17,8 +24,22 @@ function App({ initialPath = '/' }: AppProps) {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          
+          {/* Exam Routes */}
+          <Route path="exams" element={<ExamDirectory />} />
+          <Route path="exams/:slug" element={<ExamDetail />} />
+          
+          {/* Comparison Route */}
+          <Route path="best-nursing-coaching-in-india" element={<CoachingComparison />} />
+          
+          {/* Resources Routes */}
+          <Route path="nursing-exam-courses" element={<Courses />} />
+          <Route path="nursing-pyq" element={<PYQHub />} />
+          <Route path="nursing-test-series" element={<TestSeries />} />
+          
+          {/* Blog Routes */}
           <Route path="blog" element={<BlogList />} />
-          <Route path=":slug" element={<StaticPage />} />
+          <Route path="blog/:slug" element={<BlogPost />} />
         </Route>
       </Routes>
     </RouterWrapper>
